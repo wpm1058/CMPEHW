6203d20f50888b373a95b2fbaeb822415e22692b
wpm1058 <78914367+wpm1058@users.noreply.github.com>
2022-03-24 23:36:34 -0400

    Update q3logic.vhd

--- DSD2 HW9/Q3/q3logic.vhd
+++ DSD2 HW9/Q3/q3logic.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity q3logic is
 	port (
 		a,b,c : in std_logic;
-		y 	  : out std_logic;
+		y 	  : out std_logic
 	);
 end q3logic;
 
@@ -12,3 +12,4 @@ architecture dataflow of q3logic is
 begin
 	y <= (a and (not b)) or ((not b) and (not c)) or ((not a) and b and c);
 end architecture dataflow;
+
