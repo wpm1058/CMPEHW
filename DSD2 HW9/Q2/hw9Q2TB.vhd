4ff271a0ff52675d69c96aa38b39883d8040c1b0
wpm1058 <78914367+wpm1058@users.noreply.github.com>
2022-03-24 23:07:06 -0400

    Update hw9Q2TB.vhd

--- DSD2 HW9/Q2/hw9Q2TB.vhd
+++ DSD2 HW9/Q2/hw9Q2TB.vhd
@@ -11,7 +11,7 @@ end entity hw9Q2TB;
 architecture tb of hw9Q2TB is
 	signal data: std_logic_vector(3 downto 0);
 	signal segments: std_logic_vector(6 downto 0);
-	file vector_file : text open read_mode is "text.txt";
+	file vector_file : text open read_mode is "test.txt";
 begin
 	uut: entity work.sevSeg 
 		port map (
@@ -22,7 +22,7 @@ begin
 		variable vector_line : line;
 		variable vector_valid: boolean;
 		variable v_data_in : std_logic_vector(3 downto 0);
-		variable v_segments_out : std_logic_vector(7 downto 0);
+		variable v_segments_out : std_logic_vector(6 downto 0);
 	begin
 		test : while (not endfile(vector_file)) loop
 		-- ADD: read a line from the vector file
@@ -31,12 +31,12 @@ begin
 			read(vector_line,v_data_in,good => vector_valid);
 		-- exit loop if vector_valid false which happens when the line starts with #
 			next when not vector_valid;
-			a <= v_data_in;
+			data <= v_data_in;
 			wait for 10 ns;
 		-- ADD: read v_y_out, the expected output
 			read(vector_line,v_segments_out,good =>vector_valid);
 			assert (segments = v_segments_out) report "error in operation at "
-			& time'image(now) " data = " & to_hstring(data) & " and segments = "
+			& time'image(now) & " data = " & to_hstring(data) & " and segments = "
 			& to_hstring(segments);
 			wait for 10 ns;	
 		end loop test;
